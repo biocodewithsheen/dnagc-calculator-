9bb50868af27a153ebbd4a72169e4ea567322b1f
biocodewithsheen <sh1een0071gijo@gmail.com>
2026-05-29 10:52:54 +0530

    Update gccalculator.py

--- gccalculator.py
+++ gccalculator.py
@@ -1,1 +1,35 @@
-a=input("Enter a DNA sequence :")
+dna=input("Enter a DNA sequence :")
+
+# Calculating length of input DNA sequence 
+length=length(dna)
+print("The length of DNA sequence is:", length)
+
+#Converting to upper case
+dna=dna.upper()
+print(dna)
+
+#Counting A,T,G,C and N
+a=dna.count("A")
+t=dna.count("T")
+g=dna.count("G")
+c=dna.count("C")
+n=dna.count("N")
+
+#Printing the results
+print(
+      "Length=", length,"\n",
+       "A=",a,"\n",
+       "T=",t,"\n",
+       "G=",g,"\n",
+       "C=",c,"\n",
+       "N=",n,"\n" )
+
+# Calculating G+C % in the sequence 
+gc=((g+c)/length)*100
+print("GC content :",gc,"%")
+
+
+        
+       
+       
+       
