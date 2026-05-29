05c5eab0427a49b0ac252ebc50426f02ee1f8182
biocodewithsheen <sh1een0071gijo@gmail.com>
2026-05-29 11:05:41 +0530

    Update gccalculator.py

--- gccalculator.py
+++ gccalculator.py
@@ -2,7 +2,7 @@ dna=input("Enter a DNA sequence :")
 
 # Calculating length of input DNA sequence 
 length=length(dna)
-print("The length of DNA sequence is:", length)
+print("The length of DNA sequence is:",length)
 
 #Converting to upper case
 dna=dna.upper()
@@ -27,6 +27,15 @@ print(
 # Calculating G+C % in the sequence 
 gc=((g+c)/length)*100
 print("GC content :",gc,"%")
+
+#Calculating melting temperature of the oligo
+tm=(4*(g+c)+2*(a+t))
+print("The melting temperature of the given oligo is:",tm)
+
+#Finds the first occurence of AUG from 5'end
+start=dna.find("AUG")
+print("The first occurence of AUG from 5'end is",start+1)
+
 
 
         
